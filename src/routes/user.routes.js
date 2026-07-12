d5bc0b7b76c6581c60bfaff7420145e320b9823b
NITISH KUMAR <nt10nitish@gmail.com>
2026-07-12 22:53:01 +0530

    generate tokens and authenticate the user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,4 +12,9 @@ router.route("/register").post(
     registerUser
 );
 
+router.route("/login").post(loginUser);
+
+//secure route
+router.route("/logout").post(verifyJWT, logoutUser);
+
 export default router;
